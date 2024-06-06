9b6e1a0a71e3bccaf7747ae4d6a91116e7e31179
mark <markgalsworthy@hotmail.com>
2024-06-06 10:47:11 +0100

    Added new project for titleprg, also blank template project.

---
+++ gamesource/titleprg/titleprg.s
@@ -0,0 +1,16 @@
+
+
+                section panel,code_c
+                org     $0                                          ; original load address
+
+
+                ;--------------------- includes and constants ---------------------------
+                INCDIR      "include"
+                INCLUDE     "hw.i"
+
+start
+                jmp start     
+           
+           
+   
+   
